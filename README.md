3564e12c7757c34e6d0dacd83e664c4e7490d6f0
Augusto Renan <augustorenanss@gmail.com>
2022-02-15 15:59:43 -0300

    Adicionado readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# spring-boot-order-api
+
+Uma API feita com JAVA SE 11, Spring Boot, JPA/Hibernate, H2 Database, e PostgreSQL, configurada para deploy no HEROKU.
+
+## Demostração
+
+[Clique aqui](https://spring-boot-order-api.herokuapp.com/) para ver a API funcionando.
+
+## Contribuição
+Pull requests são bem vindos.
+
+## Licença 
+[MIT](https://choosealicense.com/licenses/mit/)
--- src/main/java/com/augustorenan/springbootapi/repositories/UserRepository.java
+++ src/main/java/com/augustorenan/springbootapi/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package com.augustorenan.springbootapi.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.augustorenan.springbootapi.entities.User;
@@ -8,6 +9,7 @@ import com.augustorenan.springbootapi.entities.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 	
+	@Query("SELECT * FROM tb_user WHERE email = ?1")
 	public User findUserByEmail(String email);
 
 }
